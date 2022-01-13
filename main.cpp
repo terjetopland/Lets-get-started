bf7b056a1eee2bc5e0106309b466ba4966fae6c2
Terje Topland <74875770+terjetopland@users.noreply.github.com>
2022-01-13 11:01:21 +0100

    Write comments

--- main.cpp
+++ main.cpp
@@ -8,8 +8,15 @@ using namespace std;
 
 int main()
 {
+    //This is the first statement that will be executed.
     string hello = "Hello everyone in the world!";
+
     //std::cout = "character output"
     cout << hello;
+
+    /*This is a way
+     to write several lines
+     as comments*/
+
     return 0;
 }
