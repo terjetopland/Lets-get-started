3c2613dc58db6d80889a6248869465b04af9ddd8
Terje Topland <74875770+terjetopland@users.noreply.github.com>
2022-01-13 10:31:35 +0100

    Lets get started

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <array>
+#include <iostream>
+#include <string_view>
+#include <tuple>
+#include <type_traits>
+
+using namespace std;
+
+int main()
+{
+    string hello = "Hello everyone in the world!";
+    //std::cout = "character output"
+    cout << hello;
+    return 0;
+}
